e02061ffb2e7dfb14af1dfaed81bceb8fe43245e
Philip Brown <phil@ipbrown.com>
2013-10-12 14:48:03 +0100

    Add comment block to Translate methods

--- src/WorldPay/Translate.php
+++ src/WorldPay/Translate.php
@@ -2,6 +2,14 @@
 
 class Translate {
 
+  /**
+   * Set Test Mode
+   *
+   * Convert WorldPay environment from string to integer
+   *
+   * @var $input string
+   * @return integer
+   */
   public static function setTestMode($input)
   {
     if($input == 'production'){return 0;}
@@ -9,6 +17,14 @@ class Translate {
     return 100;
   }
 
+  /**
+   * Get Test Mode
+   *
+   * Convert WorldPay environment from integer to string
+   *
+   * @var $input integer
+   * @return string
+   */
   public static function getTestMode($input)
   {
     if($input == 0){return 'production';}
