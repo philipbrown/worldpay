34abbb4987b62dcf2cd60ad676c2e826fc28bf44
Philip Brown <phil@ipbrown.com>
2013-10-12 14:28:45 +0100

    Add Helper methods

--- src/WorldPay/Helper.php
+++ src/WorldPay/Helper.php
@@ -1,3 +1,35 @@
 <?php namespace WorldPay;
 
-class Helper {}
+class Helper {
+
+  /**
+   * Convert a string to camelcase
+   *
+   * e.g hello_world -> helloWorld
+   *
+   * @var string $str
+   * @return string
+   */
+  public static function camelise($str)
+  {
+    return preg_replace_callback('/_([a-z0-9])/', function ($m) {
+        return strtoupper($m[1]);
+      },
+      $str
+    );
+  }
+
+  /**
+   * Convert a Paramater key into a full method name
+   *
+   * e.g first_name -> getFirstNameParameter
+   *
+   * @var string $type ('get'|'set')
+   * @var string $key
+   */
+  public static function convertParamMethodName($type = 'get', $key)
+  {
+    return $type.ucfirst(self::camelise($key)).'Parameter';
+  }
+
+}
