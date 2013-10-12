ea30ee68db89960dd9f813efe21b6230fcf7a186
Philip Brown <phil@ipbrown.com>
2013-10-12 12:11:01 +0100

    Add WorldPay class

---
+++ src/WorldPay/WorldPay.php
@@ -0,0 +1,41 @@
+<?php namespace Worldpay;
+
+class Worldpay {
+
+  /**
+   * @var array
+   */
+  protected $config;
+
+  /**
+   * Set Config
+   *
+   * @var array
+   * @return void
+   */
+  public function setConfig($config)
+  {
+    $this->config = $config;
+  }
+
+  /**
+   * Request
+   *
+   * @return Worldpay\Request
+   */
+  public function request($parameters = null)
+  {
+    return new Request($this->config, $parameters);
+  }
+
+  /**
+   * Response
+   *
+   * @return Worldpay\Response
+   */
+  public function response($parameters = null)
+  {
+    return new Response($this->config, $parameters);
+  }
+
+}
