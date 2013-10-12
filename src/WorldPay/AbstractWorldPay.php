9aaf6f0b45e8c3cefb7dd4897ef0e7edbbb0a850
Philip Brown <phil@ipbrown.com>
2013-10-12 14:26:58 +0100

    Add ParameterBag to AbstractWorldPay

--- src/WorldPay/AbstractWorldPay.php
+++ src/WorldPay/AbstractWorldPay.php
@@ -1,3 +1,20 @@
 <?php namespace WorldPay;
 
-abstract class AbstractWorldPay {}
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+abstract class AbstractWorldPay {
+
+  /**
+   * @var Symfony\Component\HttpFoundation\ParameterBag;
+   */
+  protected $parameters;
+
+  /**
+   * Create object with new instance of ParameterBag
+   */
+  public function __construct()
+  {
+    $this->parameters = new ParameterBag;
+  }
+
+}
