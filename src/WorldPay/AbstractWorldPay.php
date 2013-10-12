ad5526da44576dc9f1299e1a8d5d98cbfa97386d
Philip Brown <phil@ipbrown.com>
2013-10-12 14:42:41 +0100

    Add main abstract methods

--- src/WorldPay/AbstractWorldPay.php
+++ src/WorldPay/AbstractWorldPay.php
@@ -17,4 +17,107 @@ abstract class AbstractWorldPay {
     $this->parameters = new ParameterBag;
   }
 
+  /**
+   * Determines if this key has a setter or getter method
+   *
+   * @var string $type ('get'|'set')
+   * @var string $key
+   * @return bool
+   */
+  protected function hasMethod($type = 'get', $key)
+  {
+    return method_exists($this, Helper::convertParamMethodName($type, $key));
+  }
+
+  /**
+   * Is Custom Param?
+   *
+   * Determines if this is a custom parameter
+   *
+   * Custom parameters begin with 'MC_' or 'C_'
+   *
+   * @var string $key
+   * @return bool
+   */
+  protected function isCustomParam($key)
+  {
+    if(substr($key, 0, 3) == 'MC_' || substr($key, 0, 2) == 'C_')
+    {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Set Parameter
+   *
+   * Set the parameter if it has a setter method
+   * or if it is a Custom parameter.
+   *
+   * @var string $key
+   * @var mixed $value
+   * @return void
+   */
+  protected function setParameter($key, $value)
+  {
+    if($this->hasMethod('set', $key))
+    {
+      $method = Helper::convertParamMethodName('set', $key);
+      $this->{$method}($value);
+    }
+    if($this->isCustomParam($key))
+    {
+      $this->setCustomParam($key, $value);
+    }
+  }
+
+  /**
+   * Get Parameter
+   *
+   * Get the parameter if it has a getter method
+   * or if it is a Custom parameter
+   *
+   * @var string $key
+   * @return string
+   */
+  protected function getParameter($key)
+  {
+    if($this->hasMethod('get', $key))
+    {
+      $method = Helper::convertParamMethodName('get', $key);
+      return $this->{$method}();
+    }
+    if($this->isCustomParam($key))
+    {
+      return $this->getCustomParam($key);
+    }
+  }
+
+  /**
+   * Set Custom Param
+   *
+   * Called when an MC_ or C_ parameter is set
+   *
+   * @var string $key
+   * @var string $value
+   * @return void
+   */
+  protected function setCustomParam($key, $value)
+  {
+    $this->parameters->set($key, $value);
+  }
+
+  /**
+   * Get Custom Param
+   *
+   * Called when a MC_ or C_ parameter is requested
+   *
+   * @var string $key
+   * @return string
+   */
+  protected function getCustomParam($key)
+  {
+    return $this->parameters->get($key);
+  }
+
 }
