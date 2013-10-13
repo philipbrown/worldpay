780d903310f3f7c350e7436181840f6b32257af1
Philip Brown <phil@ipbrown.com>
2013-10-13 14:00:40 +0100

    Add test to check custom environment URL

--- tests/WorldPay/WorldPayTest.php
+++ tests/WorldPay/WorldPayTest.php
@@ -12,4 +12,21 @@ class WorldpayTest extends TestCase {
     $this->assertInstanceOf('Worldpay\Response', $this->getWorldpay()->response($this->getNormalResponse()));
   }
 
+  public function testEnvironementCallbackURL()
+  {
+    $wp = new WorldPay;
+    $wp->setConfig(array(
+      'env' => 'local',
+      'local' => 'example.local/callbacks/worldpay'
+    ));
+    $request = $wp->request(array(
+      'instId' => '123456',
+      'cartId' => 'my_shop',
+      'currency' => 'GBP',
+      'amount' => 9.99
+    ));
+    $prepare = $request->prepare();
+    $this->assertEquals('example.local/callbacks/worldpay', $prepare['endpoint']);
+  }
+
 }
