189209ca53fa93a836e1d8e73127589f0ff1cd8b
Philip Brown <phil@ipbrown.com>
2013-10-13 13:51:14 +0100

    Add comments and set environment

--- example.php
+++ example.php
@@ -1,7 +1,9 @@
 <?php
-
+// Require autoload
 require __DIR__.'/vendor/autoload.php';
 
+// Create new Worldpay object
 $wp = new Worldpay\Worldpay;
 
-var_dump($wp);
+// Set the environment
+$wp->setConfig(array('env' => 'development'));
