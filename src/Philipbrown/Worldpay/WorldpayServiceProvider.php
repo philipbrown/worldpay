d6ab50fd7a9e001a47c0d0fce758bea7e3c75ed2
Philip Brown <phil@ipbrown.com>
2013-10-29 12:25:28 +0000

    Fix ServiceProvider class name

--- src/Philipbrown/Worldpay/WorldpayServiceProvider.php
+++ src/Philipbrown/Worldpay/WorldpayServiceProvider.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class WorldpayProvider extends ServiceProvider {
+class WorldpayServiceProvider extends ServiceProvider {
 
   /**
    * Indicates if loading of the provider is deferred.
