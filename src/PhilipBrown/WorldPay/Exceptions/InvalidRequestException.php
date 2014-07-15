0b67edbfd6fdfa86143efc9c54906b596232c340
philipbrown <phil@ipbrown.com>
2014-07-15 18:49:34 +0100

    Create the Config object

--- src/PhilipBrown/WorldPay/Exceptions/InvalidRequestException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace PhilipBrown\WorldPay\Exceptions;
-
-use Exception;
-
-class InvalidRequestException extends Exception {}
--- src/PhilipBrown/WorldPay/Exceptions/InvalidResponseException.php
+++
@@ -1,5 +0,0 @@
-<?php namespace PhilipBrown\WorldPay\Exceptions;
-
-use Exception;
-
-class InvalidResponseException extends Exception {}
