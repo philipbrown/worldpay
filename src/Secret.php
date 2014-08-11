47c377bd8d15956d047977b68dad4e6b2fe76be6
philipbrown <phil@ipbrown.com>
2014-08-11 20:09:00 +0100

    Add the Secret value object

---
+++ src/Secret.php
@@ -0,0 +1,44 @@
+<?php namespace PhilipBrown\WorldPay;
+
+use Assert\Assertion;
+
+class Secret {
+
+  /**
+   * @var string
+   */
+  protected $secret;
+
+  /**
+   * @param string $secret
+   * @return void
+   */
+  private function __construct($secret)
+  {
+    Assertion::string($secret);
+
+    $this->secret = $secret;
+  }
+
+  /**
+   * Set the Secret
+   *
+   * @param string $secret
+   * @return PhilipBrown\WorldPay\Secret
+   */
+  public static function set($secret)
+  {
+    return new Secret($secret);
+  }
+
+  /**
+   * Return the Secret when cast to string
+   *
+   * @return string
+   */
+  public function __toString()
+  {
+    return $this->secret;
+  }
+
+}
