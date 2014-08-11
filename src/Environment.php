d8caabe8474a7ac2e1570240d8a9cdb517e078dc
philipbrown <phil@ipbrown.com>
2014-08-11 19:59:44 +0100

    Add Environment value object

---
+++ src/Environment.php
@@ -0,0 +1,56 @@
+<?php namespace PhilipBrown\WorldPay;
+
+use Assert\Assertion;
+
+class Environment {
+
+  /**
+   * @var string
+   */
+  private $env;
+
+  /**
+   * @param string $env
+   * @return void
+   */
+  private function __construct($env)
+  {
+    Assertion::string($env);
+
+    $this->env = $env;
+  }
+
+  /**
+   * Set the Environment
+   *
+   * @param string $env
+   * @return PhilipBrown\WorldPay\Environment
+   */
+  public static function set($env)
+  {
+    return new Environment($env);
+  }
+
+  /**
+   * Return the environment as an integer
+   *
+   * @return int
+   */
+  public function asInt()
+  {
+    if($this->env === 'production') return 0;
+
+    return 100;
+  }
+
+  /**
+   * Return the Environment when cast to string
+   *
+   * @return string
+   */
+  public function __toString()
+  {
+    return $this->env;
+  }
+
+}
---
+++ tests/EnvironmentTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PhilipBrown\WorldPay\Environment;
+
+class EnvironmentTest extends PHPUnit_Framework_TestCase {
+
+  /** @test */
+  public function should_create_a_new_production_environment()
+  {
+    $environment = Environment::set('production');
+
+    $this->assertInstanceOf('PhilipBrown\WorldPay\Environment', $environment);
+    $this->assertEquals(0, $environment->asInt());
+    $this->assertEquals('production', (string) $environment);
+  }
+
+  /** @test */
+  public function should_create_a_new_development_environment()
+  {
+    $environment = Environment::set('development');
+
+    $this->assertInstanceOf('PhilipBrown\WorldPay\Environment', $environment);
+    $this->assertEquals(100, $environment->asInt());
+    $this->assertEquals('development', (string) $environment);
+  }
+
+  /** @test */
+  public function should_create_a_new_local_environment()
+  {
+    $environment = Environment::set('local');
+
+    $this->assertInstanceOf('PhilipBrown\WorldPay\Environment', $environment);
+    $this->assertEquals(100, $environment->asInt());
+    $this->assertEquals('local', (string) $environment);
+  }
+
+  /** @test */
+  public function environment_should_be_a_string()
+  {
+    $this->setExpectedException('Assert\AssertionFailedException');
+
+    $environment = Environment::set(['env' => 'production']);
+  }
+
+}
