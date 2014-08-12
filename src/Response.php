8cf9ec0a439e4271f32687741ff1060a314a2158
philipbrown <phil@ipbrown.com>
2014-08-12 19:21:16 +0100

    Add the Response class

---
+++ src/Response.php
@@ -0,0 +1,92 @@
+<?php namespace PhilipBrown\WorldPay;
+
+class Response {
+
+  /**
+   * @var PhilipBrown\WorldPay\Password
+   */
+  private $password;
+
+  /**
+   * @var array
+   */
+  private $body;
+
+  /**
+   * Create a new Response instance
+   *
+   * @param PhilipBrown\WorldPay\Password $password
+   * @param array $body
+   * @return void
+   */
+  public function __construct(Password $password, array $body)
+  {
+    $this->password = $password;
+    $this->body = $body;
+  }
+
+  /**
+   * Assert if the response is valid
+   *
+   * @return bool
+   */
+  public function isValid()
+  {
+    return $this->callbackPW === (string) $this->password;
+  }
+
+  /**
+   * Assert if the request was a success
+   *
+   * @return bool
+   */
+  public function isSuccess()
+  {
+    return $this->transStatus === 'Y';
+  }
+
+  /**
+   * Assert if the request is valid
+   *
+   * @return bool
+   */
+  public function isCancelled()
+  {
+    return $this->transStatus === 'C';
+  }
+
+  /**
+   * Assert if the response is from the production environment
+   *
+   * @return bool
+   */
+  public function isProduction()
+  {
+    return $this->testMode == 0;
+  }
+
+  /**
+   * Assert if the response is from the development environment
+   *
+   * @return bool
+   */
+  public function isDevelopment()
+  {
+    return $this->testMode == 100;
+  }
+
+  /**
+   * Get the private attributes
+   *
+   * @param string $key
+   * @return mixed
+   */
+  public function __get($key)
+  {
+    if(isset($this->body[$key]))
+    {
+      return $this->body[$key];
+    }
+  }
+
+}
---
+++ tests/ResponseTest.php
@@ -0,0 +1,104 @@
+<?php
+
+use PhilipBrown\WorldPay\Response;
+use PhilipBrown\WorldPay\Password;
+
+class ResponseTest extends PHPUnit_Framework_TestCase {
+
+  /** @test */
+  public function should_create_a_new_response()
+  {
+    $response = new Response(Password::set(''), []);
+
+    $this->assertInstanceOf('PhilipBrown\WorldPay\Response', $response);
+  }
+
+  /** @test */
+  public function should_have_access_to_response_attributes()
+  {
+    $response = new Response(Password::set(''), ['name' => 'Philip Brown']);
+
+    $this->assertEquals('Philip Brown', $response->name);
+  }
+
+  /** @test */
+  public function should_be_valid_with_correct_password()
+  {
+    $response = new Response(Password::set('qwerty'), ['callbackPW' => 'qwerty']);
+
+    $this->assertTrue($response->isValid());
+  }
+
+  /** @test */
+  public function should_be_invalid_with_incorrect_password()
+  {
+    $response = new Response(Password::set('qwerty'), ['callbackPW' => 'dvorak']);
+
+    $this->assertFalse($response->isValid());
+  }
+
+  /** @test */
+  public function should_be_a_success()
+  {
+    $response = new Response(Password::set(''), ['transStatus' => 'Y']);
+
+    $this->assertTrue($response->isSuccess());
+  }
+
+  /** @test */
+  public function should_not_be_a_success()
+  {
+    $response = new Response(Password::set(''), ['transStatus' => 'C']);
+
+    $this->assertFalse($response->isSuccess());
+  }
+
+  /** @test */
+  public function should_be_cancelled()
+  {
+    $response = new Response(Password::set(''), ['transStatus' => 'C']);
+
+    $this->assertTrue($response->isCancelled());
+  }
+
+  /** @test */
+  public function should_not_be_cancelled()
+  {
+    $response = new Response(Password::set(''), ['transStatus' => 'Y']);
+
+    $this->assertFalse($response->isCancelled());
+  }
+
+  /** @test */
+  public function should_be_in_production()
+  {
+    $response = new Response(Password::set(''), ['testMode' => '0']);
+
+    $this->assertTrue($response->isProduction());
+  }
+
+  /** @test */
+  public function should_not_be_in_production()
+  {
+    $response = new Response(Password::set(''), ['testMode' => '100']);
+
+    $this->assertFalse($response->isProduction());
+  }
+
+  /** @test */
+  public function should_be_in_development()
+  {
+    $response = new Response(Password::set(''), ['testMode' => '100']);
+
+    $this->assertTrue($response->isDevelopment());
+  }
+
+  /** @test */
+  public function should_not_be_in_development()
+  {
+    $response = new Response(Password::set(''), ['testMode' => '0']);
+
+    $this->assertFalse($response->isDevelopment());
+  }
+
+}
